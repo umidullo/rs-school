b4e6c88e72ee72475a48332e603c2b903b062d36
umidullo <umidullo@suyunov.uz>
2021-03-22 11:08:54 +0500

    feat: add audio for mouse press

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,10 +1,47 @@
 const piano = document.querySelector(".piano");
 const pianoKeys = document.querySelectorAll(".piano-key");
 
+const playAudio = (src) => {
+  const audio = new Audio();
+  audio.src = src;
+  audio.currentTime = 0;
+  audio.play();
 
-piano.addEventListener('mousedown', (event) => {
+}
+
+const startSound = (event) => {
   event.target.classList.add('piano-key-active');
-})
-piano.addEventListener('mouseup', (event) => {
+  const note = event.target.dataset.note;
+  const src = `assets/audio/${note}.mp3`;
+  playAudio(src);
+}
+
+const stopSound = (event) => {
   event.target.classList.remove('piano-key-active');
-})
+}
+
+const startCorrespondOver = (event) => {
+  if (event.target.classList.contains('piano-key')) {
+    event.target.classList.add('piano-key-active');
+  };
+  pianoKeys.forEach((elem) => {
+    elem.addEventListener('mouseover', startSound)
+    elem.addEventListener('mouseout', stopSound)
+  });
+
+}
+
+const stopCorrespondOver = (elem) => {
+  elem.target.classList.remove('piano-key-active')
+  pianoKeys.forEach((elem) => {
+    elem.removeEventListener('mouseover', startSound)
+    elem.removeEventListener('mouseout', stopSound)
+  });
+}
+
+pianoKeys.forEach((elem) => {
+  elem.addEventListener('mousedown', startSound)
+});
+
+piano.addEventListener('mousedown', startCorrespondOver);
+piano.addEventListener('mouseup', stopCorrespondOver);
